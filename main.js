957fc76d56adadaae15857b3c6d5274e453bcce8
AnsariAhmedFrontEnd <am27043@gmail.com>
2022-11-18 21:46:24 +0530

    DOM

---
+++ main.js
@@ -0,0 +1,12 @@
+// let items = document.querySelector('.title');
+
+// items.style.color = 'green';
+
+// items.innerHTML = '<b>Add Items</b>'
+
+
+let add = document.getElementsByClassName('.title');
+
+
+
+add.style.fontWeight = 'bold';
